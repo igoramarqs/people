ad413fa96faa4208b388a48b2fde79ab62320878
Igor (wee) <devweee@gmail.com>
2023-02-14 11:22:58 -0300

    putting everything in its proper place. created new component file to store people list data

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import PeopleListItem from './PeopleListItem';
 
 const PeopleList = props => {
-    const { peoples } = props
-    const textElements = peoples.map(people => {
-        const { first } = people.name;
-        return <Text key={ first }>{ first }</Text>;
-    });
+    const { peoples } = props;
+
+    const items = peoples.map(people => 
+        <PeopleListItem 
+            key={people.name.first} 
+            people={people}
+        />
+    );
 
     return (
-        <View>
-            { textElements }
+        <View style={styles.container}>
+            { items }
         </View>
     )
 
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e2f9ff'
+    },
+})
 
 export default PeopleList;
