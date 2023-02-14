f592149729f50d43929472201797382997bb0d82
Igor (wee) <devweee@gmail.com>
2023-02-14 12:05:33 -0300

    stylized avatar image and first finished screen

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 import { capitalizeFirstLetter } from '../util';
 
@@ -8,6 +8,7 @@ const PeopleListItem = props => {
     const { title, first, last } = people.name;
     return (
         <View style={styles.line}>
+            <Image style={styles.avatar} source={{ uri: people.picture.thumbnail }} />
             <Text style={styles.lineText}>
                 { `${
                     capitalizeFirstLetter(title)
@@ -32,7 +33,15 @@ const styles = StyleSheet.create({
     },
     lineText: {
         fontSize: 20,
-        paddingLeft: 15        
+        paddingLeft: 15,
+        flex: 7
+    },
+    avatar: {
+        aspectRatio: 1,
+        flex: 1,
+
+        marginLeft: 15,
+        borderRadius: 50
     }
 });
 
