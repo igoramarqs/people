3e1ca4cb248980b28e40740aa26ac307e1cbb18c
Igor (wee) <devweee@gmail.com>
2023-02-14 09:55:52 -0300

    rendering a list with array

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header';
 
-export default function App() {
-  return (
-    <View>      
-        <Header title="Pessoas!" />        
-    </View>
-  );
+export default class App extends React.Component {
+
+  renderList() {
+    const names = [
+      'Igor',
+      'Brunna',
+      'Késia',
+      'John Doe'
+    ];
+
+    const textElements = names.map(name => {
+      return <Text key={name}>{name}</Text>
+    })
+
+    return textElements;
+  }  
+
+  render() {
+    return(
+      <View>
+        <Header title="Pessoas!"/>
+        { this.renderList() }
+      </View>
+    );
+  }
+
 }
